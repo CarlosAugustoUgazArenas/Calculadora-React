4984536486a52a874c0b0600c39d8418a449c2ea
CarlosUgaz <nova9gp@gmail.com>
2023-07-26 21:30:47 -0500

    a

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css"
+import "./index.css"
+import Calculadora from "./Calculate";
+
+function App() {
+  
+  return (
+    <>
+      {/* <h1>Hello</h1> */}
+      <Calculadora></Calculadora>
+    </>
+  );
+}
+
+export default App;
